4ebdb9309dbf51163b491617651ef97072618303
Sachin Gupta <sachin.gupta.dsp@gmail.com>
2025-03-25 18:22:57 +0530

    Initial commit

---
+++ minimal_ray_example/problem_helper.py
@@ -0,0 +1,27 @@
+import functools
+import ray
+
+# Decorator 
+def test(f):
+    print(f"{f.__name__} registered")
+    @functools.wraps(f)
+    def wrapper(*args, **kwargs):
+        wrapper.some_attribute = 42
+        f(*args, **kwargs)
+        return wrapper
+    return wrapper
+
+# Sample function: to be executed in a remote process
+@test
+def sample_func():
+    print("This is a test function")
+
+# Utility function
+def run(custom_func: callable = None):
+    ray.init()
+
+    remote_actor = ray.remote(custom_func)
+    sample_out = ray.get(remote_actor.remote())
+
+    # Check if the attribute is available
+    print(f"{sample_out.some_attribute=}")
---
+++ minimal_ray_example/problem_main.py
@@ -0,0 +1,7 @@
+# Minimal example: Working scenario
+# Some source in a helper file
+from problem_helper import run
+from problem_helper import sample_func
+
+if __name__ == "__main__":
+    run(sample_func)
