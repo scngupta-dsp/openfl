bbd55fa0b3271310558719cc6d1236c9bcf87268
Sachin Gupta <sachin.gupta.dsp@gmail.com>
2025-03-28 14:28:04 +0530

    Modified __module__ of custom_func

--- minimal_ray_example/problem_helper.py
+++ minimal_ray_example/problem_helper.py
@@ -19,7 +19,9 @@ def sample_func():
 # Utility function
 def run(custom_func: callable = None):
     ray.init()
-
+    print(f"{custom_func.__module__=}")
+    custom_func.__module__ = "global"
+    print(f"{custom_func.__module__=}")
     remote_actor = ray.remote(custom_func)
     sample_out = ray.get(remote_actor.remote())
 
